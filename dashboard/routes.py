7933b1cec9b4e822dbaa6a60de38893224aefbf0
Nirmal <nirmal.sr2025cse@sece.ac.in>
2026-05-17 22:06:02 +0530

    Removed windows tesseract path

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -11,7 +11,6 @@ import pytesseract
 
 dashboard = Blueprint('dashboard',__name__)
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 @dashboard.route('/home')
 def home():
